59605d11ba28ede5a302229dc8e954d2cbd8beee
OshiniChange8 <oshini@change8.co.uk>
2023-10-24 02:28:01 +0530

    Completed Quiz component

--- src/main/java/com/osh/Quizapp/controller/QuestionController.java
+++ src/main/java/com/osh/Quizapp/controller/QuestionController.java
@@ -3,6 +3,9 @@ package com.osh.Quizapp.controller;
 import com.osh.Quizapp.entity.Question;
 import com.osh.Quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,17 +18,18 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
     }
+    //ResponseEntity --> To return both data and StatusCode
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question question){
+    public ResponseEntity<String> addQuestion(@RequestBody Question question){
         return questionService.addQuestion(question);
     } // @RequestBody --> convert json data into object
 
